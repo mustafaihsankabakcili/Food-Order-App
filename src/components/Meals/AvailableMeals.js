ad0267146a67ca396e6f202737b8515d3229da10
Mustafa Ihsan Kabakcili <mustafaihsankabakcili@gmail.com>
2022-06-16 14:24:32 +0200

    first commit

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,75 @@
+import { Fragment, useEffect, useState } from "react";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
+
+const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [httpError, setHttpError] = useState(false);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      setIsLoading(true);
+      const response = await fetch(
+        "https://project-17-c3bfe-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <Fragment>
+      {isLoading ? (
+        <section className={classes.MealsLoading}>
+          <p>Loading...</p>
+        </section>
+      ) : httpError ? (
+        <section className={classes.MealsError}>
+          <p>Failed to fetch..</p>
+        </section>
+      ) : (
+        <section className={classes.meals}>
+          <Card>
+            <ul>{mealsList}</ul>
+          </Card>
+        </section>
+      )}
+    </Fragment>
+  );
+};
+
+export default AvailableMeals;
